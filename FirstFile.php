25644568c48b3086751af86126834761108de34b
shapemyself <liu_hs_vip@126.com>
2015-09-18 15:49:07 +0800

    first test for git

---
+++ FirstFile.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuhengsheng
+ * Date: 15/9/18
+ * Time: 下午3:47
+ */
+class MyTestClass{
+    public function main(){
+        echo "just test for github";
+    }
+}
